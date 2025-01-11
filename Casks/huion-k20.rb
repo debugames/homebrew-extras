ce4e3b515424f8332d1db6849e8b78120626e55b
horaguy <157380662+horaguy@users.noreply.github.com>
2025-01-11 21:53:50 +0900

    Remove verified field from Huion K20 cask URL

--- Casks/huion-k20.rb
+++ Casks/huion-k20.rb
@@ -2,8 +2,7 @@ cask "huion-k20" do
   version "15.6.5.71"
   sha256 "e410c2f133edeb37d93fbf68a53262eb468e3a71555ec8af4433842bac471c69"
 
-  url "https://driverdl.huion.com/driver/K20_K100/HuionTablet_MacDriver_v#{version}.dmg",
-      verified: "driverdl.huion.com/"
+  url "https://driverdl.huion.com/driver/K20_K100/HuionTablet_MacDriver_v#{version}.dmg"
   name "Huion K20"
   desc "Driver for Huion K20"
   homepage "https://www.huion.com/jp/index.php?m=content&c=index&a=lists&catid=16&myform=1&down_title=k20"
