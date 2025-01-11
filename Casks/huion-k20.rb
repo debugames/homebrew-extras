f934efd0ed37e247ee1c55ab96545d8b833a4950
horaguy <157380662+horaguy@users.noreply.github.com>
2025-01-11 21:44:06 +0900

    Add cask for Huion K20 driver version 15.6.5.71

---
+++ Casks/huion-k20.rb
@@ -0,0 +1,28 @@
+cask "huion-k20" do
+  version "15.6.5.71"
+  sha256 "e410c2f133edeb37d93fbf68a53262eb468e3a71555ec8af4433842bac471c69"
+
+  url "https://driverdl.huion.com/driver/K20_K100/HuionTablet_MacDriver_v#{version}.dmg",
+      verified: "download01.xp-pen.com/"
+  name "Huion K20"
+  desc "Driver for Huion K20"
+  homepage "https://www.huion.com/jp/index.php?m=content&c=index&a=lists&catid=16&myform=1&down_title=k20"
+
+  livecheck do
+    url "https://www.huion.com/jp/index.php?m=content&c=index&a=lists&catid=16&myform=1&down_title=k20"
+    regex(/MacDriver_v(\d+(?:\.\d+)*)\.dmg/i)
+  end
+
+  depends_on macos: ">= :sierra"
+
+  #uninstall launchctl: "com.huion.HuionTablet",
+  #          quit:      "com.huion.HuionTablet",
+  #          pkgutil:   "com.huion.HuionTablet"
+
+  #zap trash: [
+  #  "~/Library/Saved Application State/com.huion.HuionTablet.savedState/",
+  #]
+
+  #caveats <<~EOS
+  #EOS
+end
