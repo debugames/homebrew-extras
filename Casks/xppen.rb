1c31b4a00f942ec10206a09632b610d5d782acf2
horaguy <157380662+horaguy@users.noreply.github.com>
2025-04-19 07:32:30 +0900

    Simplify and fix xppen cask URL and date parsing

--- Casks/xppen.rb
+++ Casks/xppen.rb
@@ -2,7 +2,7 @@ cask "xppen" do
   version "2025,04,4.0.8_250414"
   sha256 "c792fc0c5df8c38b75267d302d0c3ced49371ac7be73998875279bc627dad3ca"
 
-  url "https://download01.xp-pen.com/file/#{version.csv.first}/#{version.csv.second}/XPPenMac_#{version.csv.third}.zip",
+  url "https://download01.xp-pen.com/file/#{version.csv.second}/#{version.csv.third}/XPPenMac_#{version.csv.first}.zip",
       verified: "download01.xp-pen.com/"
   name "XPPen"
   desc "Driver for XP-Pen tablets"
@@ -12,9 +12,8 @@ cask "xppen" do
     url "https://www.xp-pen.jp/download-978.html"
     strategy :page_match do |page|
       version = page[/class="name_text".*?>XPPenMac_(.*?)</, 1]
-      date = page[/class="name_time".*?>(\w{3} \d{2},\d{4})/, 1]
-      date = Date.parse(date)
-      "#{date.year},#{date.month.to_s.rjust(2, "0")},#{version}"
+      date = Date.parse(page[/class="name_time".*?>(\w{3} \d{2},\d{4})/, 1])
+      "#{version},#{date.year},#{date.month.to_s.rjust(2, "0")}"
     end
   end
 
