476602a987b86096ea58c65881e30253c5188b74
horaguy <157380662+horaguy@users.noreply.github.com>
2025-04-19 11:31:08 +0900

    Refactor bump-cask script and update workflow env vars

--- .github/scripts/bump-cask.sh
+++ .github/scripts/bump-cask.sh
@@ -2,7 +2,7 @@
 
 # Usage: bump-cask.sh <package> [version]
 
-BREW_TAP_NAME="debugames/extras"
+BREW_TAP_NAME="$(brew tap)"
 
 function main(){
   CASKNAME="$BREW_TAP_NAME/$1"
@@ -20,20 +20,17 @@ function main(){
 function create_merge_bump_pr() {
   CASKNAME=$1
   VERSION=$2
-  brew bump-cask-pr "$CASKNAME" --no-fork --version "$VERSION" > tmp.log 2>&1 || ret=$?
-  cat tmp.log
+  brew bump-cask-pr "$CASKNAME" --no-fork --version "$VERSION" > tmp.log 2>&1; ret=$?; cat tmp.log
   # If the command-log includes specific message, return 0. Otherwise, return the error code
-  if [ -n "$ret" ] && [ "$ret" -ne 0 ]; then
-    if grep -q "Duplicate PRs must not be opened." tmp.log; then
-      echo "PR for $VERSION already exists. Skipping."
-      exit 0
-    elif grep -q "nothing to commit, working tree clean" tmp.log; then
-      echo "No changes (Maybe because this is the first PR for this cask)."
+  if [ "$ret" -ne 0 ]; then
+    if grep -q "Duplicate PRs must not be opened." tmp.log \
+      || grep -q "pull requests are duplicates" tmp.log \
+      || grep -q "nothing to commit, working tree clean" tmp.log; then
       exit 0
     fi
     exit "$ret"
   fi
-  # Find branch from the log and find PR number, then merge it
+  # Find branch & PR from log, and merge it
   BRANCH=$(grep -oE "branch '[^']+" -m 1 tmp.log | sed "s/branch '//")
   PR=$(gh pr list --head "$BRANCH" --json number --jq ".[0].number")
   gh pr merge "$PR" --merge --delete-branch
