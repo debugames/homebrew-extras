2702bbb0e246a38460503ed9ff27930f3998176e
horaguy <157380662+horaguy@users.noreply.github.com>
2025-04-19 10:42:31 +0900

    Refactor cask bump scripts and workflow steps

--- .github/scripts/bump-cask.sh
+++ .github/scripts/bump-cask.sh
@@ -1,30 +1,42 @@
 #!/usr/bin/env bash
 
-# Create PR for bumping cask, and merge it if success
+# Usage: bump-cask.sh <caskname> [version]
 
-PACKAGE=$1 # <username>/<tapname>/<caskname>
-VERSION=$2 # new version of the cask
+BREW_TAP_NAME="debugames/extras"
 
-brew bump-cask-pr $PACKAGE --no-fork --version $VERSION > tmp.log 2>&1
-ret=$?
-cat tmp.log
-# If the command failed and the log contains 'Duplicate PRs must not be opened.', return 0
-# Otherwise, return the error code
-if [ $ret -ne 0 ]; then
-  if grep -q "Duplicate PRs must not be opened." tmp.log; then
-    echo "PR for $VERSION already exists. Skipping."
-    exit 0
-  elif grep -q "nothing to commit, working tree clean" tmp.log; then
-      echo "No changes. This happens when the PR of this cask has not yet been opened even once."
-    exit 0
-  else
-    exit $ret
+function main(){
+  PACKAGE="$BREW_TAP_NAME/$1"
+  VERSION=$2
+  if [ -z "$VERSION" ]; then
+    VERSION=$(brew livecheck --json "$PACKAGE" | jq -r '.[0].version.latest')
+    if [ -z "$VERSION" ]; then
+      echo "Failed to get latest version"
+      exit 1
+    fi
   fi
-fi
+  create_merge_bump_pr "$PACKAGE" "$VERSION"
+}
 
-# Find branch from the log and find PR number, then merge it
-BRANCH=$(grep -oE "branch '[^']+" -m 1 tmp.log | sed "s/branch '//")
-PR=$(gh pr list --head "$BRANCH" --json number --jq ".[0].number")
-gh pr merge $PR --merge --delete-branch
+function create_merge_bump_pr() {
+  PACKAGE=$1
+  VERSION=$2
+  brew bump-cask-pr "$PACKAGE" --no-fork --version "$VERSION" > tmp.log 2>&1 || ret=$?
+  cat tmp.log
+  # If the command-log includes specific message, return 0. Otherwise, return the error code
+  if [ -n "$ret" ] && [ "$ret" -ne 0 ]; then
+    if grep -q "Duplicate PRs must not be opened." tmp.log; then
+      echo "PR for $VERSION already exists. Skipping."
+      exit 0
+    elif grep -q "nothing to commit, working tree clean" tmp.log; then
+      echo "No changes (Maybe because this is the first PR for this cask)."
+      exit 0
+    fi
+    exit "$ret"
+  fi
+  # Find branch from the log and find PR number, then merge it
+  BRANCH=$(grep -oE "branch '[^']+" -m 1 tmp.log | sed "s/branch '//")
+  PR=$(gh pr list --head "$BRANCH" --json number --jq ".[0].number")
+  gh pr merge "$PR" --merge --delete-branch
+}
 
-exit 0
+main "$@"
